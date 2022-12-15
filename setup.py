22c4fed7986eed920c92be10aff2cd74af4ea1f3
arunkurhade <arun.kurhade2121@gmail.com>
2022-12-15 06:52:12 +0530

    new file were added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="arun.kurhade2121@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
