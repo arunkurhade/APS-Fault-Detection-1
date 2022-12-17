b10c8816abf0c7e6b0e641eb0f33184f43ad716d
arunkurhade <arun.kurhade2121@gmail.com>
2022-12-17 11:44:31 +0530

    updated

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from typing import List
 REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT = "-e ."
 
-def get_requirements()->List[Str]:
+def get_requirements()->List[str]:
     with open (REQUIREMENT_FILE_NAME) as requirement_file:
-        reqquirement_list = requirement_file.readlines()
-    requirement_list = [requirement_name.replace("\n","")] for requirement_name in requirement_list
-    if HYPHEN_E_DOT in requirment_list:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
 
@@ -17,7 +17,7 @@ def get_requirements()->List[Str]:
 
 setup(
     name="sensor",
-    version="0.0.1",
+    version="0.0.2",
     author="ineuron",
     author_email="arun.kurhade2121@gmail.com",
     packages=find_packages(),
